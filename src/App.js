2460dafd8f15b840d3667c6ce9aac9a39bddee04
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2021-02-03 11:29:27 +0200

    local csv file on console.log

--- src/App.js
+++ src/App.js
@@ -1,42 +1,70 @@
 import React, { Component } from "react";
-import Test from "./components/Test.js";
+import Exercise from "./components/Exercise.js";
 import "./Styles.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import Papa from "papaparse";
+import csvFile from "./HistoricalQuotes.csv";
+
+const api_link =
+  "https://www.nasdaq.com/api/v1/historical/AAPL/stocks/2020-01-20/2021-01-20";
+
 export default class App extends Component {
+  constructor() {
+    super(); 
+    this.csvDataFromFile = this.csvDataFromFile.bind(this);
+
+    this.state = {
+      data: [],
+      download: false,
+    };
+  }
+
+  componentDidMount() {
+    this.csvDataFromFile(); 
+  }
+
+  csvDataFromFile(){
+    Papa.parse(csvFile, {
+      download: true,
+      complete: function (input) {
+        const records = input.data;
+        console.log(records);
+      },
+    });
+  }
+
   render() {
     return (
       <div>
         <Router>
-         
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/users">Users</Link>
-                </li>
-              </ul>
-            </nav>
+          <nav>
+            <ul className="exerciseList">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
 
-            <Switch>
-              <Route path="/about">
-                <Test text={"about123"} />
-              </Route>
+          <Switch>
+            <Route path="/about">
+              <Exercise text={"about123"} />
+            </Route>
 
-              <Route path="/users">
-                <Test text={"users123"} />
-              </Route>
+            <Route path="/users">
+              <Exercise text={"users123"} />
+            </Route>
 
-              <Route path="/">
-                <Test text={"homse, grove street"} />
-              </Route>              
-            </Switch>
-          
+            <Route path="/">
+              <Exercise text={"homse, grove street"} />
+            </Route>
+          </Switch>
         </Router>
       </div>
     );
--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Test extends Component {   
+export default class Exercise extends Component {   
 
     render() {
         return (

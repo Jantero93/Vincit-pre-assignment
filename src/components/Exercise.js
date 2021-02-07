fadafb1f9199847e6ec5ae642a8855324f185918
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2021-02-07 09:14:43 +0200

    2a completed

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import ShowResult from "./ShowResult.js";
+
+// showresult class renders exercises based on these consts
+const exercise1A = 0;
+const EXERCISE2A = 1;
+const exercise2B = 2;
+const exercose3C = 3; 
 
 export default class Exercise extends Component {
   constructor(props) {
@@ -7,15 +14,14 @@ export default class Exercise extends Component {
     this.startDateChangeHandler = this.startDateChangeHandler.bind(this);
     this.endDateChangeHandler = this.endDateChangeHandler.bind(this);
     this.upwardTrend = this.upwardTrend.bind(this);
-    this.highestTradingVolume = this.highestTradingVolume.bind(this);
-    this.exerciseTwo_B_b = this.exerciseTwo_B_b.bind(this);
-    this.exerciseThree = this.exerciseThree.bind(this);
+    this.highestTradingVolume = this.highestTradingVolume.bind(this)
+    this.SMA5 = this.SMA5.bind(this);
 
     this.state = {
-      exercisesResult: null,
+      exerciseResult: null,
+      exerciseNumber: null,
       startDate: null,
-      endDate: null,
-      //invalidDate: false,
+      endDate: null     
     };
   }
 
@@ -42,66 +48,154 @@ export default class Exercise extends Component {
   }
 
   upwardTrend() {
-    //for shorter variable name
-    const data = this.props.data;
-    //set clocks on .csv and local time on same so can <= and >= work correctly
-    const startDay = this.offSetTime(this.state.startDate);
-    const endDay = this.offSetTime(this.state.endDate);
-    
-    //filter days before start day and after end day
-    var result = data.filter(
-      (csvLine) => csvLine.Date >= startDay && csvLine.Date <= endDay
+    var result = this.filterExceedingData(
+      this.state.startDate,
+      this.state.endDate,
+      this.props.data
     );
-    
 
-    //array begins with oldest date
+    //filter days before start day and after end day
     result = result.reverse();
-    
+
+    //broilerplate
+    if (
+      this.state.startDate === null ||
+      this.state.endDate === null ||
+      this.state.startDate > this.state.endDay
+    ) {
+      this.setState({ exerciseResult: "invalid date(s)" });
+      return;
+    }
 
     var biggestStreak = 0;
-    var currentStreak = 1;
+    var currentStreak = 0; // after first compare -> two days row
 
     console.log(result);
 
-    // comparing to next element
-    
+    // comparing to next element, no need check last one
+
+    var IndexEndOfStreak;
+
     for (var i = 0; i < result.length - 1; i++) {
-      // if next day bigger -> streak++
-      if (result[i]["Close/Last"] < result[i+1]["Close/Last"]) {
-        currentStreak++; if (currentStreak > biggestStreak) biggestStreak = currentStreak;
-        console.log("striik kasvo, päiväys, ", result[i].Date, "i: ", i);
-      }
-      else {
-        currentStreak = 1;
+      // if next day bigger -> streak++, else streak = 0
+
+      if (result[i]["Close/Last"] < result[i + 1]["Close/Last"]) {
+        currentStreak++;
+
+        if (currentStreak > biggestStreak) {
+          biggestStreak = currentStreak;
+          //compare to next one
+          IndexEndOfStreak = i + 1;
+          //console.log("indeksi putken päässä", IndexEndOfStreak);
+        }
+      } else {
+        currentStreak = 0;
       }
     }
 
-    console.log(biggestStreak);
-    this.setState({exercisesResult: biggestStreak});
-    
+    var dateEnd = new Date(result[IndexEndOfStreak].Date);
+    var dateStart = new Date(result[IndexEndOfStreak - biggestStreak].Date);
+
+    dateEnd = dateEnd.toLocaleDateString("en-US");
+    dateStart = dateStart.toLocaleDateString("en-US");
+
+    const renderedResult =
+      "Close/Last price increased " +
+      (biggestStreak + 1).toString() +
+      " day(s) in a row between " +
+      dateStart +
+      " and " +
+      dateEnd;
+
+    //console.log(biggestStreak, "index: ", IndexEndOfStreak);
+    // biggestStreak = biggestStreak.toString() + " day(s) upward trend";
+    this.setState({
+      exerciseResult: renderedResult,
+      exerciseNumber: EXERCISE2A,
+    });
   }
 
+  //fixing clocktime on csv dates and browswer dates
   offSetTime(oldDate) {
     var date = new Date(oldDate);
-    date.setHours(date.getHours()-2);
+    date.setHours(date.getHours() - 2);
     return date;
   }
 
+  //cut out data outside time range and fixes browser times on same timezone
+  filterExceedingData(){
+    //set clocktimes  on .csv and local time on same so <= and >= work correctly
+    const startDay = this.offSetTime(this.state.startDate);
+    const endDay = this.offSetTime(this.state.endDate);
+
+    return this.props.data.filter(
+      (csvLine) => csvLine.Date >= startDay && csvLine.Date <= endDay
+    );  
+  }
+
   highestTradingVolume() {
-    console.log("click 2B a");
-  }
+    console.log("click 2B a");   
+    const data = this.filterExceedingData();
 
-  exerciseTwo_B_b() {
-    console.log("click 2B b");
-  }
+    var hiTradeingVolume = 0; //highest tradeing volume
+    var dateTradeVolume = 0; // date on highest tradeing volume
+    var hiValueChange = 0; // highest value change within day
+    var dateValueChange = 0; // date on highest value change within day
 
-  exerciseThree() {
+    console.log("data: ",data)
+    //change wihin day difference on high and low prices 
+    for (const csvLine of data) {
+
+      if (Number(csvLine.Volume) > Number(hiTradeingVolume)) {
+        hiTradeingVolume = csvLine.Volume;
+        dateTradeVolume = csvLine.Date;
+      }
+
+      /*
+      console.log("korkein tradevolue:", hiTradeingVolume , 
+      "\n korkein volume date: ", dateTradeVolume,
+      "\n korkein arvomuutos: ", hiValueChange,
+      "\n date korkeimmalle muutokselle: ", dateValueChange);
+      */
+     
+
+      const difWihinDay = csvLine.High - csvLine.Low;
+      console.log("forloop: ", difWihinDay, "  ", csvLine.Date)
+      if (Number(hiValueChange) < Number(difWihinDay)) {
+        hiValueChange = difWihinDay;
+        dateValueChange = csvLine.Date;
+      }
+    }
+
+
+    console.log("lopullinen korkeis",hiTradeingVolume);
+    console.log("arvo muutos ja pvm: ", hiValueChange, "  ", dateValueChange);
+    /*
+    pyöristä vielä arvomuutos ja vastausten palauttaminen jotenkin fiksusti?
+    */
+  } 
+
+  SMA5() {
     console.log("click 3");
+
+    var data = this.filterExceedingData();
+    data = data.reverse(); // oldest date first
+
+    for (var i = 0; i < data.length; i++) {
+
+      // if no five 
+      if (i - 5 < 0) {
+
+      }
+
+    }
+    this.setState({exerciseResult: <div>testi12345678</div>});
+
   }
 
   render() {
     console.log("exercise state", this.state);
-    //  console.log("exercise props", this.props);
+    //  console.log("exercise props", this.props);    
 
     return (
       <div>
@@ -128,17 +222,13 @@ export default class Exercise extends Component {
           <button type="button" onClick={this.highestTradingVolume}>
             The highest trading volume
           </button>
-          <br />
-          <button type="button" onClick={this.exerciseTwo_B_b}>
-            Stock price change within day
-          </button>
-          <br />
-          <button type="button" onClick={this.exerciseThree}>
+          <br />         
+          <button type="button" onClick={this.SMA5}>
             SMA 5
           </button>
         </div>
         <br></br>
-        <div>Result: {this.state.exercisesResult}</div>
+       <div> <ShowResult result={this.state.exerciseResult} exerciseNumber={this.state.exerciseNumber} /> </div>
       </div>
     );
   }
---
+++ src/components/ShowResult.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+
+export default class ShowResult extends Component {
+    render() {
+        return (
+            <div>
+                MOIKKULI
+            </div>
+        )
+    }
+}
